3396d940eb7fbd472977f447ac760c65f7c880bd
Dindaa <39338346+Dindaa@users.noreply.github.com>
2018-05-26 17:54:14 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+import { Marker } from 'react-native-maps';
+export default class App extends Component {
+
+  state = {
+    region: {
+      latitude: -8.149407,
+      longitude: 115.216667,
+      latitudeDelta: 0.8922,
+      longitudeDelta: 0.8421,
+    },
+    markers : [
+      {
+        key:1,
+        latlng: {
+          latitude:-8.638698,
+          longitude: 115.217550
+        },
+        title: 'Hotel Nikki',
+        subtitle: 'Hotel nikki adalah hotel yang indah dipusat kota denpasar,Bali .'
+      },
+      {
+        key:2,
+        latlng: {
+          latitude:-8.111813,
+          longitude: 115.085306
+        },
+        title: 'Hotel Garuda',
+        subtitle: 'Hotel garuda adalah salah satu hotel yang terletak di kabupaten buleleng, Bali.'
+      },
+      {
+        key:3,
+        latlng: {
+          latitude:-8.111813,
+          longitude: 115.085306
+        },
+        title: 'Hotel Aston',
+        subtitle: 'Hotel aston adalah hotel yang terletak di kota denopasar, Bali.'
+      }
+   ]
+  };
+
+  render() {
+    return (
+      <View style={styles.contMain}>
+        <View style={styles.contHeader}>
+            <Text style={styles.textHeader}>
+              Hotel di Bali
+            </Text>
+        </View>
+        <View style={styles.contMaps}>
+              <MapView
+                style={styles.map}
+                region={this.state.region}
+              >
+              {this.state.markers.map(mark => (
+              <Marker
+                  key = {mark.key}
+                  coordinate={mark.latlng}
+                  title={mark.title}
+                  description={mark.subtitle}
+                />
+              ))}
+              </MapView>
+        </View>
+        <View style={styles.contFooter}>
+           <Text style={styles.textFooter}> copyright@Dindaa</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  contMain: {
+      flex : 1
+  },
+  contHeader: {
+    backgroundColor: '#0057E7',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    position: 'relative'
+  },
+  contMaps : {
+    flex : 10
+  },
+  textHeader: {
+    fontSize: 20,
+    color: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  contFooter: {
+    backgroundColor: '#0057E7',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    position: 'relative'
+  },
+  textFooter: {
+    fontSize: 16,
+    color: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+
+});
